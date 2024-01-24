1d28556f1160efdab65cb94b91a605d15798ac28
ah0556702 <ah0556702@otc.edu>
2024-01-24 09:24:09 -0600

    Initial Commit

---
+++ app/src/main/java/com/example/practiceapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.practiceapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); // R = res, layout = res->layout, activity_main = res->layout->activity_main.xml
+    }
+}
+
